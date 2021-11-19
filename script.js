bac84368153e66ad403e15c9397313352cb940c7
Kjartan Óli Ágústsson <kjartanoli@disroot.org>
2021-11-19 16:51:34 +0000

    Add in-ram cache of TODOs, and their associated DOM elements
    Keeping references to the various TODO entries, and the associated DOM elements
    allows for easier manipulation of the associated data, and makes saving to local
    storage more straight forward.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import { el } from "./lib/helpers.js";
-import { addCategory, getCategories, createGategoryCreator } from "./lib/categories.js";
+import { addCategory, getCategories, createCategoryCreator } from "./lib/categories.js";
+import { cache } from "./lib/cache.js";
 
 let takki = document.querySelector("#testTakki");
 let iTitill = document.querySelector("#inputTitill");
@@ -13,7 +14,6 @@ let iLitur = document.querySelector("#inputLitur");
 let sideBarCat = document.querySelector(".flokkarData")
 
 let tagUpperLimit = 5;
-let sizeMagicNum = 0; //fjöldi stafa sem þarf að taka aftan af input til þess að textinn lýti út fyrir að vera í miðjunni
 
 let listi = [];
 let id = 0;
@@ -27,143 +27,11 @@ let id = 0;
 //   builder(uppl.ID,uppl.Titill,uppl.Lysing,uppl.Dags,uppl.Flokkur,uppl.Tags,uppl.Litur);
 // }
 
-function builder(id,tit,lys,dag, category ,tags,lit) {
-  let clone = template.content.cloneNode(true);
-  let title = clone.querySelector(".titill");
-  let description = clone.querySelector(".lysing");
-  let date = clone.querySelector(".dags");
-  let tagContainer = clone.querySelector(".tags");
-  const categorySelect = clone.querySelector(".category");
-  let addTag = clone.querySelector(".addTag")
-  let checkbox = clone.querySelector(".check")
-
-  title.value = tit;
-  resizeTitle(title)
-  title.addEventListener("change",(e) => {
-    console.log(title.value)
-    resizeTitle(title)
-  });
-
-  description.value = lys;
-  resizeDescription(description)
-  description.addEventListener("input",(e) => {
-    // console.log(description.value)
-    resizeDescription(description)
-  });
-
-
-  date.value = dag;
-
-  let card = clone.querySelector(".card");
-	for (const cat of getCategories()) {
-		const c = el("option", cat.title);
-		c.setAttribute("value", cat.id);
-		if (cat.id === category) {
-			c.setAttribute("selected", "true");
-		}
-		categorySelect.appendChild(c);
-	}
-
-	const newCat = el("option", "Nýr flokkur");
-	newCat.setAttribute("value", "new");
-	categorySelect.appendChild(newCat);
-
-	categorySelect.addEventListener("change", () => {
-		console.log(categorySelect.value)
-		if (categorySelect.value === "new") {
-			createGategoryCreator(categorySelect)()
-		}
-	})
-
-  card.style.borderColor = lit
-  let idToDel = id;
-
-  for (const tag of tags) {
-    let t = el("input")
-    let li = el("li", t)
-    t.value = tag;
-    resizeTag(t)
-    tagContainer.appendChild(li);
-    t.addEventListener("change",(e) => {
-      console.log(t.value);
-      resizeTag(t)
-      if (t.value === "") li.remove();
-    })
-	}
-
-  addTag.addEventListener("click",(e) => {
-    if (tagContainer.childElementCount < tagUpperLimit) {
-      let t = el("input")
-      let li = el("li", t)
-      t.setAttribute("placeholder","tag")
-      t.setAttribute("size","10")
-      t.addEventListener("change",(e) => {
-        console.log(t.value);
-        resizeTag(t)
-        if (t.value === "") li.remove();
-      })
-      tagContainer.appendChild(li);
-    }
-  })
-
-  checkbox.addEventListener('change', (e) => {
-    if (checkbox.checked) {
-      console.log("Checkbox is checked..");
-    } else {
-      console.log("Checkbox is not checked..");
-    }
-  });
-
-  clone.querySelector("#delete").addEventListener("click",(e) => {
-    card.remove()
-    console.log(idToDel)
-    for (const item of listi) {
-      if (item.ID === idToDel) {
-        localStorage.removeItem("listi")
-        listi.splice(listi.indexOf(item),1)
-        localStorage.setItem("listi",JSON.stringify(listi))
-        break;
-      }
-    }
-  })
-
-  holder.appendChild(clone)
-}
 
 function takeAndEscape(e) {
   let output = e.value;
   e.value = null;
   return output;
-}
-
-function writeJson() {
-  if (listi.length <= 0) {
-    id = 0;
-    console.log(listi.length)
-  }
-  else{
-    id = listi[listi.length-1].ID+1;
-    console.log(id)
-  }
-  let obj = {
-    ID: id,
-    Titill: takeAndEscape(iTitill),
-    Lysing: takeAndEscape(iLysing),
-    Dags: dateFormat(takeAndEscape(iDags)),
-    Flokkur: takeAndEscape(iFlokkur),
-    Tags: [
-      takeAndEscape(iTag1),
-      takeAndEscape(iTag2),
-      takeAndEscape(iTag3),
-    ],
-    Litur: takeAndEscape(iLitur),
-  }
-
-  localStorage.removeItem("listi")
-  listi.push(obj)
-  localStorage.setItem("listi",JSON.stringify(listi))
-
-  return obj;
 }
 
 async function onStart() {
@@ -184,11 +52,8 @@ async function onStart() {
 
 	const categories = localStorage.getItem("categories")
 	if (categories === null) {
-		console.log(cats)
 		for (const cat of cats) {
 			addCategory(cat);
-      let nafn = cat.title;
-      console.log(nafn)
 		}
 	}
 	else {
@@ -196,45 +61,18 @@ async function onStart() {
 			addCategory(cat);
 		}
 	}
-	console.log(items)
-	console.log(getCategories())
   for (const item of items) {
     // listi.push(key);
     // builder(key.ID,key.Titill,key.Lysing,key.Dags,key.Flokkur,key.Tags,key.Litur)
-    if (!item.deleted) {
-      builder(item.id, item.title, item.description, item.date, item.category, item.tags, "#000000")
-    }
+		const element = cache.newItem(item);
+		if (element) {
+			holder.appendChild(element);
+		}
+    // if (!item.deleted) {
+    //   builder(item.id, item.title, item.description, item.date, item.category, item.tags, "#000000")
+    // }
   }
 }
 
-function resizeTitle(t) {
-  if (t.value.length > 32) {
-    t.setAttribute("rows",2)
-    t.style.height = "2.4em"
-  }
-  else {
-    t.setAttribute("rows",1);
-    t.style.height = "1.2em"
-  }
-}
-
-function resizeDescription(d) {
-  if (d.value == "") {
-    d.style.height = "2em"
-  }
-  else {
-    d.style.height = Math.floor(d.value.length / 45) + 2 +"em"
-  }
-}
-
-function resizeTag(t) {
-  if (t.value.length > sizeMagicNum) {
-    t.setAttribute("size",t.value.length-sizeMagicNum)
-  }
-
-  else {
-    t.setAttribute("size",1)
-  }
-}
 
 onStart();
