a98e80f58ac46aa878b813e3d5a3de64a75e2344
Tumi <thorvaldur.tumi@gmail.com>
2021-11-23 14:45:48 +0000

    BIG commit :)

--- script.js
+++ script.js
@@ -1,72 +1,82 @@
 import { cache } from "./lib/cache.js";
-import { addCategory, getCategories, showCategories } from "./lib/categories.js";
-
-let takki = document.querySelector("#testTakki");
-let iTitill = document.querySelector("#inputTitill");
-let iLysing = document.querySelector("#inputLysing");
-let iDags = document.querySelector("#inputDags");
-let iTag1 = document.querySelector("#inputTag1");
-let iTag2 = document.querySelector("#inputTag2");
-let iTag3 = document.querySelector("#inputTag3");
-let iFlokkur = document.querySelector("#inputFlokkur");
-let iLitur = document.querySelector("#inputLitur");
-let sideBarCat = document.querySelector(".flokkarData")
-
-let listi = [];
-let id = 0;
-
-// takki.addEventListener("click", nyttShit);
-// let template = document.querySelector("#template");
-// let holder = document.querySelector("#holder");
-
-// function nyttShit() {
-//   let uppl = writeJson();
-//   builder(uppl.ID,uppl.Titill,uppl.Lysing,uppl.Dags,uppl.Flokkur,uppl.Tags,uppl.Litur);
-// }
-
-function takeAndEscape(e) {
-  let output = e.value;
-  e.value = null;
-  return output;
-}
+import {
+	addCategory,
+	getCategories,
+	showCategories,
+} from "./lib/categories.js";
+import { el } from "./lib/helpers.js";
 
 async function onStart() {
-  // let geymdurListi = localStorage.getItem("listi");
-  // let parsedListi = JSON.parse(geymdurListi);
-	let items, cats;
-	if (localStorage.getItem("listi") === null || localStorage.getItem("categories")) {
-		try {
-			let data = await fetch(new URL("data.json", window.location.href));
-			data = await data.json();
-			items = data.items;
-			cats = data.categories;
-		}
-		catch (e) {
-			console.log(e);
-		}
+	// let geymdurListi = localStorage.getItem("listi");
+	// let parsedListi = JSON.parse(geymdurListi);
+	let remote, cats;
+	let local = JSON.parse(localStorage.getItem("todos"));
+	
+	try {
+		let data = await fetch(new URL("data.json", window.location.href));
+		data = await data.json();
+		remote = data.items;
+		cats = data.categories;
+	} catch (e) {
+		console.log(e);
 	}
+	
 
-	const categories = localStorage.getItem("categories")
+	const categories = localStorage.getItem("categories");
 	if (categories === null) {
 		for (const cat of cats) {
 			addCategory(cat);
 		}
-	}
-	else {
+	} else {
 		for (const cat of JSON.parse(categories)) {
 			addCategory(cat);
 		}
 	}
-	console.log(items)
-	console.log(getCategories())
-  showCategories();
-
-  for (const item of items) {
-		const element = cache.newItem(item);
-		if (element) {
-			holder.appendChild(element);
+	if (local) {
+		for (const localItem of local) {
+			let element;
+			for (const item of remote) {
+				if (item.id === localItem.id) {
+					element = cache.newItem(item.modified > localItem.modified ? item : localItem);
+					if (element) {
+						holder.appendChild(element);
+					}
+					break;
+				}
+			}
+			if (!element) {
+				element = cache.newItem(localItem);
+				console.log(element)
+				if (element) {
+					holder.appendChild(element);
+				}
+			}
 		}
-  }
+	} else {
+		for (const item of remote) {
+			const element = cache.newItem(item);
+			if (element) {
+				holder.appendChild(element);
+			}
+		}
+	}
+	showCategories();
+
+	createAddNew()
+}
+
+function createAddNew() {
+	let takki = el("button","Bæta við nýju ToDo");
+	takki.setAttribute("class","addNewButton");
+	takki.addEventListener("click", (e) => {
+		console.log("CreateAddNew")
+		let newCard = cache.newItem(null);
+		takkiHolder.replaceWith(newCard);
+		createAddNew();
+	})
+	let takkiHolder = el("div",takki)
+	takkiHolder.setAttribute("class","takkiholder")
+	document.querySelector("#holder").appendChild(takkiHolder)
 }
 
 onStart();
